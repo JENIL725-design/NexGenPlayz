a0a45e060ee7ee20128894406da21127f93dd0e1
JENILMAKWANA1 <jenilmakwana2@gmail.com>
2025-09-25 20:36:15 +0530

    Add files via upload

--- nexgenplayz_db.sql
+++ nexgenplayz_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 15, 2025 at 02:18 PM
+-- Generation Time: Sep 25, 2025 at 05:05 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -36,6 +36,26 @@ CREATE TABLE `achievements` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `admin_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password_hash` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`admin_id`, `username`, `email`, `password_hash`) VALUES
+(1, 'JENIL', 'JENIL@gmail.com', '102030');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `games`
 --
 
@@ -43,8 +63,51 @@ CREATE TABLE `games` (
   `game_id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
   `cover_image` varchar(100) NOT NULL,
-  `video_preview` varchar(100) NOT NULL
+  `video_preview` varchar(100) NOT NULL,
+  `price` decimal(10,2) NOT NULL DEFAULT 0.00
 ) ENGINE=InnoDB DEFAULT CHARSET=dec8 COLLATE=dec8_bin;
+
+--
+-- Dumping data for table `games`
+--
+
+INSERT INTO `games` (`game_id`, `title`, `cover_image`, `video_preview`, `price`) VALUES
+(9, 'FORZA 5', 'img/Forza5.png', 'video/FORZA 480.mp4', 79.99),
+(10, 'BLACK MYTH', 'img/Black.png', 'video/BLACK MYTH 480.mp4', 89.99),
+(11, 'BLACK OPS 6', 'img/Blackops6.png', 'video/Call of Duty Black Ops 6 480.mp4', 59.99),
+(12, 'CYBERPUNK 2077', 'img/2077.jpg', 'video/Cyberpunk 2077 480.mp4', 79.99),
+(13, 'Assassin\'s Creed Brotherhood', 'img/Brother.png', 'video/Assassin\'s Creed Brotherhood 480.mp4', 69.99),
+(14, 'Apex Legends ', 'img/apex.png', 'video/APEXLEGEND 480.mp4', 49.99),
+(15, 'Battlefield 2042', 'img/2042.png', 'video/Battlefiled 2042.mp4', 52.99),
+(16, 'GTA 6', 'img/gta 6.png', 'video/GTA VI 480.mp4', 99.99),
+(17, 'ILL', 'img/ill.png', 'video/ILL 480.mp4', 37.99),
+(18, 'JUST CAUSE 4', 'img/JustCause4.png', 'video/JUST CAUSE 4 480.mp4', 38.99),
+(19, 'SEKIRO ', 'img/Sekiro.png', 'video/Sekiro 480.mp4', 49.99);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `payment_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `game_id` int(11) NOT NULL,
+  `card_number` varchar(16) NOT NULL,
+  `card_holder` varchar(255) NOT NULL,
+  `expiry` varchar(5) NOT NULL,
+  `cvv` varchar(4) NOT NULL,
+  `payment_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `payments`
+--
+
+INSERT INTO `payments` (`payment_id`, `user_id`, `game_id`, `card_number`, `card_holder`, `expiry`, `cvv`, `payment_date`) VALUES
+(6, 6, 9, '5798321475863259', 'USER GAMER', '2345', '7846', '2025-09-25 14:20:29'),
+(7, 6, 16, '3574869158723548', 'PRO GAMER', '8753', '247', '2025-09-25 15:01:28');
 
 -- --------------------------------------------------------
 
@@ -60,6 +123,13 @@ CREATE TABLE `support_messages` (
   `message` text NOT NULL,
   `submitted_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=dec8 COLLATE=dec8_bin;
+
+--
+-- Dumping data for table `support_messages`
+--
+
+INSERT INTO `support_messages` (`message_id`, `name`, `email`, `subject`, `message`, `submitted_at`) VALUES
+(1, 'some', 'something@gmail.com', 'app', 'we need your app', '2025-09-15 15:58:14');
 
 -- --------------------------------------------------------
 
@@ -82,7 +152,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `email`, `password_hash`, `profile_picture`, `tagline`, `created_at`) VALUES
-(6, 'A PERSON WHO THINKS ALL THE TIME', 'jenil@gmail.com', 'jenil', 'uploads/profile_photos/profile_68af207b764af.avif', '', '2025-08-27 15:12:59');
+(6, 'GAMING PRO', 'jenil@gmail.com', 'jenil', 'uploads/profile_photos/profile_68d559657edcb.png', '', '2025-09-25 15:01:57'),
+(7, 'something', 'something@gmail.com', '123', '', '', '2025-09-15 14:59:44');
 
 -- --------------------------------------------------------
 
@@ -105,9 +176,17 @@ CREATE TABLE `user_achievements` (
 
 CREATE TABLE `user_owned_games` (
   `ownership_id` int(11) NOT NULL,
-  `user_id` varchar(100) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `game_id` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=dec8 COLLATE=dec8_bin;
+
+--
+-- Dumping data for table `user_owned_games`
+--
+
+INSERT INTO `user_owned_games` (`ownership_id`, `user_id`, `game_id`) VALUES
+(26, 6, '9'),
+(27, 6, '16');
 
 -- --------------------------------------------------------
 
@@ -136,10 +215,25 @@ ALTER TABLE `achievements`
   ADD PRIMARY KEY (`achievement_id`);
 
 --
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`admin_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- Indexes for table `games`
 --
 ALTER TABLE `games`
   ADD PRIMARY KEY (`game_id`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`payment_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `game_id` (`game_id`);
 
 --
 -- Indexes for table `support_messages`
@@ -182,22 +276,34 @@ ALTER TABLE `achievements`
   MODIFY `achievement_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `games`
 --
 ALTER TABLE `games`
-  MODIFY `game_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `game_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `support_messages`
 --
 ALTER TABLE `support_messages`
-  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `user_achievements`
@@ -209,13 +315,24 @@ ALTER TABLE `user_achievements`
 -- AUTO_INCREMENT for table `user_owned_games`
 --
 ALTER TABLE `user_owned_games`
-  MODIFY `ownership_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ownership_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `user_stats`
 --
 ALTER TABLE `user_stats`
   MODIFY `stat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `payments_ibfk_2` FOREIGN KEY (`game_id`) REFERENCES `games` (`game_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
